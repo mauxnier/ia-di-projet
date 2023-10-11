d535e39a83e2e957790a8147e6842fc4e7f80cc0
Killian Monnier <mauxnier@gmail.com>
2023-10-11 08:20:44 +0200

    ajout etape 1 et 2

---
+++ data_indexing.py
@@ -0,0 +1,34 @@
+from elasticsearch import Elasticsearch
+from data_loading import all_flow_data
+
+# Initialize Elasticsearch client
+es = Elasticsearch()
+
+# Specify the Elasticsearch index and document type
+index_name = 'flow_data_index'
+doc_type = 'flow'
+
+# Index the flow data into Elasticsearch
+for flow in all_flow_data:
+    es.index(index=index_name, doc_type=doc_type, body=flow, id=None, pipeline=None, refresh=True, routing=None, timeout=None, version=None, version_type=None, request_timeout=None)
+
+# You can now search and retrieve data from Elasticsearch as needed
+# For example, search for all flows with a source port of 80
+# Note that the port field is stored as a string, so we need to use a string query
+# See https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-query-string-query.html
+
+# Define the query
+query = {
+    "query": {
+        "query_string": {
+            "query": "port:80"
+        }
+    }
+}
+
+# Execute the query
+results = es.search(index=index_name, body=query)
+
+# Print the results
+print("Nombre de résultats: ", results['hits']['total']['value'])
+print("Premier résultat: ", results['hits']['hits'][0]['_source'])
---
+++ data_loading.py
@@ -0,0 +1,36 @@
+import os
+import glob
+from lxml import etree
+
+# Define a function to parse an XML file and convert it to a list of dictionaries
+def parse_xml_file(xml_file):
+    flow_data = []
+    tree = etree.parse(xml_file)
+    root = tree.getroot()
+
+    for flow_element in root:
+        flow_dict = {}
+        for child in flow_element:
+            flow_dict[child.tag] = child.text
+        flow_data.append(flow_dict)
+
+    return flow_data
+
+# Specify the directory containing the XML files
+xml_files_dir = '/path/to/xml/files'
+
+# Get a list of all XML files in the directory
+xml_files = glob.glob(os.path.join(xml_files_dir, '*.xml'))
+
+# Initialize an empty list to store all flow data
+all_flow_data = []
+
+# Iterate through each XML file and parse it
+for xml_file in xml_files:
+    flow_data = parse_xml_file(xml_file)
+    all_flow_data.extend(flow_data)
+
+# Now, all_flow_data contains a list of dictionaries, where each dictionary corresponds to a single flow
+# You can access and manipulate the data as needed
+print("Longueur: ", len(all_flow_data))
+# print("Premier élément: ", all_flow_data[0])
